f902ef22d56dbd63ec5cf4f96c6b6a471aa53206
Stephens6623 <stephenspargo134@gmail.com>
2025-07-06 20:00:25 -0400

    library costructed

--- client.js
+++ client.js
@@ -1,1 +1,76 @@
-// Your code goes here
+import Library from "./library.js";
+
+
+const collection = new Library("mongodb://localhost:27017", "library", "books");
+
+
+
+  await collection.test(); // Optional, just shows connection
+
+  const booksCursor = await collection.allBooks();
+
+    // Iterate through the cursor and print each book's details
+  const books = await booksCursor.toArray();
+  books.forEach(book => {
+    console.log(`Title: ${book.title}, Author: ${book.author}, Copies: ${book.copies}`);
+  });
+
+
+// Example of finding a specific book by ID
+const bookID = "6866e2660e807f6bf532702c";
+const findOneBook = await collection.findOneBook(bookID);
+if (findOneBook) {
+  console.log(`Found book: Title: ${findOneBook.title}, Author: ${findOneBook.author}, Copies: ${findOneBook.copies}`);
+}else {
+  console.log(`Book with ID ${bookID} not found.`);
+}
+
+
+// Example of finding many books with a specific query
+const query = { author: "Stephen King" };
+const findManyBooks = await collection.findManyBooks(query);
+
+await findManyBooks.forEach(book => {
+  console.log(`Found book: Title: ${book.title}, Author: ${book.author}, Copies: ${book.copies}`);
+});
+
+// Example of adding a new book to the collection
+ const newBook = {
+     title: "The Shining",
+     author: "Stephen King",
+     copies: 1
+ };
+
+ await collection.addBook(newBook);
+ const allBooksCursor = await collection.allBooks();
+ const allBooks = await allBooksCursor.toArray();
+
+ console.log("All books in the collection:");
+ allBooks.forEach(book => {
+  console.log(`Title: ${book.title}, Author: ${book.author}, Copies: ${book.copies}`);
+});
+
+// example of changing a book's name, author, and copies
+const bookIdToChange = "6866f58d5f02a2b1d254a718";
+
+const newInfo = {
+    title: "Goodnight Moon",
+    author: "Margaret Wise Brown",
+    copies: 2
+};
+await collection.changeBook(bookIdToChange, newInfo);
+const updatedBook = await collection.findOneBook(bookIdToChange);
+if (updatedBook) {
+  console.log(`Updated book: Title: ${updatedBook.title}, Author: ${updatedBook.author}, Copies: ${updatedBook.copies}`);
+}
+
+// removing a book from collection
+const bookRemoveId = "686b05eb355ca8a1901ebcd3";
+await collection.removeBook(bookRemoveId);
+
+
+ 
+
+
+
+ 
--- library.js
+++ library.js
@@ -1,1 +1,76 @@
-// Your code goes here
+import { MongoClient}  from "mongodb";
+import { ObjectId } from "mongodb";
+//Library class to manage book collection in MongoDB
+class Library {
+    constructor(dbUrl, dbName, collName) {
+      this.dbUrl = dbUrl;
+      this.dbName = dbName;
+      this.collName = collName;
+      this.dbClient;
+      
+    }
+    // Method to connect to the MongoDB database
+async client() {
+        console.log(`Connecting to ${this.dbUrl}...`)
+        this.dbClient = await MongoClient.connect(this.dbUrl)
+        console.log("Connected to database.");
+        return this.dbClient;
+    }
+    // method to test the connection
+    async test() {
+        const client = await this.client();
+        client.close();
+        console.log("Connection closed.");
+    }
+    // Method to get the collection from the database
+    async collection() {
+        const client = await this.client();
+        const db = client.db(this.dbName);
+        const collection = db.collection(this.collName);
+        return collection;
+      }
+    // Method to retrieve all books from the collection
+    async allBooks() {
+        const collection = await this.collection();
+        return collection.find({})
+    }
+    // Method to find a book by its ID
+    async findOneBook(id) {
+        const docId = new ObjectId(id);
+        const collection = await this.collection();
+        return collection.findOne({_id: docId});
+
+    }
+    // Method to find many books
+    async findManyBooks(query) {
+        const collection = await this.collection();
+        return collection.find(query);
+    }
+
+    // Method to add a new book to the collection
+     async addBook(info) {
+         const collection = await this.collection();
+         const result = await collection.insertOne(info);
+         console.log('Book added successfully!');
+         return result;
+     }
+ 
+    // Method to change a book's information
+    async changeBook(id, newInfo) {
+        const mongoId = new ObjectId(id);
+        const infoObj = { $set: newInfo };
+        const collection = await this.collection();
+        const result = await collection.updateOne({ _id: mongoId }, infoObj);
+        console.log('Book information updated successfully!');
+        return result;
+    }
+
+    // Method to remove a book from the collection
+    async removeBook(id) {
+        const mongoId = new ObjectId(id);
+        const collection = await this.collection();
+        await collection.deleteOne({ _id: mongoId });
+        console.log('Book removed successfully!');
+    }
+}
+export default Library;
